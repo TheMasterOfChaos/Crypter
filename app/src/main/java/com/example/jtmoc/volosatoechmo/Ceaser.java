a71cc83aa3a4c6135410c3c435b3e0249129d3f0
JtMoC <bir.german18@gmail.com>
2018-02-11 03:30:52 +0300

    Ceaser + Vizhner

--- app/src/main/java/com/example/jtmoc/volosatoechmo/Ceaser.java
+++ app/src/main/java/com/example/jtmoc/volosatoechmo/Ceaser.java
@@ -4,25 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity {
+public class Ceaser extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_ceaser);
     }
+
     public void crypt(View view) {
-        EditText mess = (EditText) findViewById(R.id.editText5);
-        EditText cryp = (EditText) findViewById(R.id.editText4);
-        TextView cMess = (TextView) findViewById(R.id.cMess);
+        EditText mess = (EditText) findViewById(R.id.editText10);
+        Spinner spin = (Spinner) findViewById(R.id.spin);
+        TextView cMess = (TextView) findViewById(R.id.ceaMess);
         String s1 = mess.getText().toString().toLowerCase();
-        String s2 = cryp.getText().toString().toLowerCase();
+        char z = (char) (spin.getSelectedItemPosition());
         String s3 = " ";
         char x;
         for (int i = 0; i < s1.length(); i++) {
-            x = (char) (s1.charAt(i) + s2.charAt(i % s2.length()) - 'a' + 1);
+            x = (char) (s1.charAt(i) + z);
             if (x > 'z') x -= 26;
             s3 += String.valueOf(x);
         }
--- app/src/main/java/com/example/jtmoc/volosatoechmo/MainActivity.java
+++ app/src/main/java/com/example/jtmoc/volosatoechmo/MainActivity.java
@@ -16,4 +16,8 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Main2Activity.class);
         startActivity(intent);
     }
+    public void Legion(View view) {
+        Intent intent = new Intent(this, Ceaser.class);
+        startActivity(intent);
+    }
 }
